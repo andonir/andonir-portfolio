21bc51eb36281fed406cab41f3aff07b6baa51d6
andonir <andoniroura@outlook.com>
2025-04-23 14:10:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode, useContext } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { ContextProvider } from "./context/context1.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/andonir-portfolio/">
       <ContextProvider>
         <App />
       </ContextProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
